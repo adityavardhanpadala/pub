b70cecb7392ae746eeaa2cbb9ee29ad8ecf9df1f
Aditya Vardhan <44111127+adityavardhanpadala@users.noreply.github.com>
2023-09-22 11:55:07 -0400

    Create sample2.java

---
+++ sample2.java
@@ -0,0 +1,24 @@
+                            while (true) {
+                                int $i42 = $r15.length;
+                                $i4 = $i42;
+                                androidintSinkClass.intsinkMet($i42);
+                                androidintSinkClass.intsinkMet($i2);
+                                androidintSinkClass.intsinkMet($i3);
+                                androidintSinkClass.intsinkMet($i4);
+                                if ($i3 >= $i4) {
+                                    $r12 = null;
+                                    break;
+                                }
+                                sinkClass.sinkMet($i3);
+                                $i4 = $r15[$i3].m168id();
+                                if ($i4 == $i2) {
+                                    $r4 = this.mForm;
+                                    sinkClass.sinkMet($i3);
+                                    String $r123 = $r15[$i3].value();
+                                    $r1.set($r4, $r123);
+                                    sinkClass.sinkMet($i3);
+                                    $r12 = $r15[$i3].value();
+                                    break;
+                                }
+                                $i3++;
+                            }
